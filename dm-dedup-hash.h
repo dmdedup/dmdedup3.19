d0c7a791b494034be57b0f989a7cf44c86be6877
venktesh <venktesh.bolla@gmail.com>
2016-12-21 18:12:27 +0530

    fixed bio & shash interface issue for latest kernels and supports backward compatibility

--- dm-dedup-hash.h
+++ dm-dedup-hash.h
@@ -13,10 +13,43 @@
 #ifndef DM_DEDUP_HASH_H
 #define DM_DEDUP_HASH_H
 
+#include <linux/version.h>
+
 #define DEDUP_HASH_DESC_COUNT 128
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,6,0) //4.6.0-rc1
+#define SHASH 1
+#define crypto_alloc_hash crypto_alloc_shash
+#define crypto_hash_final crypto_shash_final
+#define crypto_hash_init crypto_shash_init
+#define crypto_hash_update crypto_shash_update
+#define crypto_hash_digestsize crypto_shash_digestsize
+#define crypto_free_hash crypto_free_shash
+#define hash_desc shash_desc
+#endif
+
+static inline int crypto_has_hash(const char *alg_name, u32 type, u32 mask)
+{
+        type &= ~CRYPTO_ALG_TYPE_MASK;
+        mask &= ~CRYPTO_ALG_TYPE_MASK;
+        type |= CRYPTO_ALG_TYPE_SHASH;
+        mask |= CRYPTO_ALG_TYPE_HASH_MASK;
+
+        return crypto_has_alg(alg_name, type, mask);
+}
+
 struct hash_desc_table {
+#if SHASH
+	union{
+		/*Made as pointer from array to extra data allocation for context/state*/
+		struct shash_desc *shd;//[DEDUP_HASH_DESC_COUNT];
+		/*Added void pointer for better access, to increment byte wise*/
+		void *desc;
+	};
+	uint32_t desc_state_size;
+#else
 	struct hash_desc desc[DEDUP_HASH_DESC_COUNT];
+#endif
 	bool free_bitmap[DEDUP_HASH_DESC_COUNT];
 	atomic_long_t slot_counter;
 } /*desc_table*/;
